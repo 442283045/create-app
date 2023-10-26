19d2a350068be48245e28c5a84432b7e9b0a1c4e
Dinosaur <442283045@qq.com>
2023-10-26 23:11:51 +0800

    feat: add publish script

---
+++ publish.ps1
@@ -0,0 +1,8 @@
+# Set the npm registry to https://registry.npmjs.org/
+npm config set registry https://registry.npmjs.org/
+
+# Publish the package with public access
+npm publish --access public
+
+# Set the npm registry back to https://registry.npmmirror.com
+npm config set registry https://registry.npmmirror.com
