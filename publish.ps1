2e4318e50b3fd4b52c1f8121c6e18d7075139ad3
Dinosaur <442283045@qq.com>
2023-11-07 00:13:34 +0800

    feat: configure proxy when publish to npm

--- publish.ps1
+++ publish.ps1
@@ -2,9 +2,10 @@
 npm run build
 # Set the npm registry to https://registry.npmjs.org/
 npm config set registry https://registry.npmjs.org/
-
+npm config set https-proxy http://127.0.0.1:7890
 # Publish the package with public access
 npm publish --access public
 
 # Set the npm registry back to https://registry.npmmirror.com
 npm config set registry https://registry.npmmirror.com
+npm config delete https-proxy
